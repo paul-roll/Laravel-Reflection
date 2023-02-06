e53fbeca5fd0f84eb22228864995ebb962904b86
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-02-06 17:03:29 +0000

    restructure again, now using symlink for public

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -12,3 +14,4 @@ export default defineConfig({
         }),
     ],
 });
+
