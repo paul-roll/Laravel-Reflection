1d0bf8b26ba98b86f7438225d87ac6719045f5b7
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-02-14 09:01:36 +0000

    Fixed home view when accessed from login routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('home', ['companyCount' => Company::count(), 'employeeCount' => Employee::count()]);
     }
 }
