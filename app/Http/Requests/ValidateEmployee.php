490fe1051223c2425971eea18e4d539db4fa5072
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-02-16 15:48:52 +0000

    added regex to employee name validations

--- app/Http/Requests/ValidateEmployee.php
+++ app/Http/Requests/ValidateEmployee.php
@@ -14,8 +14,8 @@ class ValidateEmployee extends FormRequest
     public function rules()
     {
         return [
-            "first" => ['required', 'max:255'],
-            "last" => ['required', 'max:255'],
+            "first" => ['required', 'regex:"^[a-zA-Z ,.\'-]+$"', 'max:255'],
+            "last" => ['required', 'regex:"^[a-zA-Z ,.\'-]+$"', 'max:255'],
             "company_id" => ['nullable', 'exists:companies,id'],
             "email" => ['nullable', 'email:rfc,dns', 'max:255'],
             "phone" => ['nullable', 'regex:"^(\+\d{1,3}[\s.-]?)?\(?\d{3,5}\)?[\s.-]?\d{3}[\s.-]?\d{3,4}$"'],
