2dc00d71eb8988c08077c0634949bb8efb492e0d
Paul Roll <57689893+paul-roll@users.noreply.github.com>
2023-02-17 18:59:01 +0000

    adjusted regex to support a phone code without +

--- app/Http/Requests/ValidateEmployee.php
+++ app/Http/Requests/ValidateEmployee.php
@@ -18,7 +18,7 @@ class ValidateEmployee extends FormRequest
             "last" => ['required', 'regex:"^[a-zA-Z ,.\'-]+$"', 'max:255'],
             "company_id" => ['nullable', 'exists:companies,id'],
             "email" => ['nullable', 'email:rfc,dns', 'max:255'],
-            "phone" => ['nullable', 'regex:"^(\+\d{1,3}[\s.-]?)?\(?\d{3,5}\)?[\s.-]?\d{3}[\s.-]?\d{3,4}$"'],
+            "phone" => ['nullable', 'regex:"^(\+{0,1}\d{1,3}[\s.-]?)?\(?\d{3,5}\)?[\s.-]?\d{3}[\s.-]?\d{3,4}$"'],
         ];
     }
 }
